f3070f863aac5c1c0c28adfbd3820fae4b516b20
Stefan Horst <stefan.horst@grandcentrix.net>
2019-06-01 21:25:27 +0200

    moved to apt package

--- items.py
+++ items.py
@@ -1,17 +1,4 @@
 pkg_apt = {
-    "curl": {},
-    'git': {},
-    "grep": {},
-    "gzip": {},
-    "hostname": {},
-    "htop": {},
-    'tmux': {},
-    'vim': {},
-    'zsh': {},
-    'unzip': {},
-
-    # TODO: cron-apt
-
     # remove unused packages
     'nano': {
         "installed": False,
--- metadata.py
+++ metadata.py
@@ -6,3 +6,17 @@ def add_check_mk_tags(metadata):
         metadata['check_mk']['tags'] += ['deb', ]
 
     return metadata, DONE
+
+@metadata_processor
+def add_apt_packages(metadata):
+    if node.has_bundle("apt"):
+        metadata.setdefault('apt', {})
+        metadata['apt'].setdefault('packages', {})
+
+        for package in ['cron-apt', 'curl', 'ca-certificates', 'git',
+                "grep", "gzip", "hostname", "htop", 'tmux',
+                'vim', 'zsh', 'unzip']:
+
+            metadata['apt']['packages'][package] = {'installed': True}
+
+    return metadata, DONE
