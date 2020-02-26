a3c8737263bb1677adef07fe7815c304d2aacdee
Stefan Horst <stefan@ultrachaos.de>
2020-02-26 08:43:53 +0100

    new check_mk

--- metadata.py
+++ metadata.py
@@ -2,10 +2,11 @@
 def add_check_mk_tags(metadata):
     if node.has_bundle('check_mk_agent'):
         metadata.setdefault('check_mk', {})
-        metadata['check_mk'].setdefault('tags', [])
-        metadata['check_mk']['tags'] += ['deb', ]
+        metadata['check_mk'].setdefault('tags', {})
+        metadata['check_mk']['tags']['dist'] = 'deb'
 
     return metadata, DONE
+
 
 @metadata_processor
 def add_apt_packages(metadata):
