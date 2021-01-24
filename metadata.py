9fa72a0bb7d2e48cbf3b0863149b785203a28a41
Stefan Horst <stefan@ultrachaos.de>
2021-01-24 22:19:46 +0100

    move release_names from apt to debian

--- metadata.py
+++ metadata.py
@@ -1,39 +1,43 @@
-@metadata_reactor
-def add_check_mk_tags(metadata):
-    if not node.has_bundle("check_mk_agent"):
-        raise DoNotRunAgain
+defaults = {}
 
-    return {
-        'check_mk': {
-            'tags': {
-                'dist': 'deb',
-            }
+# if we use check_mk, add the dist tag
+if node.has_bundle("check_mk_agent"):
+    defaults['check_mk'] = {
+        'tags': {
+            'dist': 'deb',
         }
     }
 
+# if we install apt, install default packages
+if node.has_bundle("apt"):
+    defaults['apt'] = {
+        'packages': {
+            'cron-apt': {'installed': True},
+            'curl': {'installed': True},
+            'ca-certificates': {'installed': True},
+            'git': {'installed': True},
 
-@metadata_reactor
-def add_apt_packages(metadata):
-    if not node.has_bundle("apt"):
-        raise DoNotRunAgain
+            "grep": {'installed': True},
+            "gzip": {'installed': True},
+            "hostname": {'installed': True},
+            "htop": {'installed': True},
+            'tmux': {'installed': True},
 
-    return {
-        'apt': {
-            'packages': {
-                'cron-apt': {'installed': True},
-                'curl': {'installed': True},
-                'ca-certificates': {'installed': True},
-                'git': {'installed': True},
-
-                "grep": {'installed': True},
-                "gzip": {'installed': True},
-                "hostname": {'installed': True},
-                "htop": {'installed': True},
-                'tmux': {'installed': True},
-
-                'vim': {'installed': True},
-                'zsh': {'installed': True},
-                'unzip': {'installed': True},
-            }
+            'vim': {'installed': True},
+            'zsh': {'installed': True},
+            'unzip': {'installed': True},
         }
     }
+
+release_names = {
+    8: 'jessie',
+    9: 'stretch',
+    10: 'buster',
+    11: 'bullseye',
+    12: 'bookworm',
+}
+
+# set release_name
+defaults['debian'] = {
+    'release_name': release_names.get(node.os_version[0], 'jessie'),
+}
